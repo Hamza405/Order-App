5db426342b02f0e086a7e6a01e5e6e29a7cd576a
hamza <hamzash863@gmail.com>
2022-07-08 23:03:30 +0300

    Implement Meals components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsDetails from "./MealsDetails";
+import AvailableMeals from "./AvailableMealis";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsDetails />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
