8fd9892465803bf163e4238ebc091ab74404f657
hamza <hamzash863@gmail.com>
2022-07-10 12:00:00 +0300

    animat cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import style from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cartContext";
 
 const HeaderCartButton = (props) => {
   const cartContext = useContext(CartContext);
+  const [animatedButton, setAnimatedButton] = useState(false);
+
   const items = Array.isArray(cartContext.items)
     ? cartContext.items.reduce((number, item) => {
         return number + item.amount;
       }, 0)
     : 0;
+
+  const buttonStyle = `${style.button} ${animatedButton && style.bump}`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) return;
+    setAnimatedButton(true);
+
+    const timer = setTimeout(() => {
+      setAnimatedButton(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={style.button} onClick={props.showCart}>
+    <button className={buttonStyle} onClick={props.showCart}>
       <span className={style.icon}>
         <CartIcon />
       </span>
