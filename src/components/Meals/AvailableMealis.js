a785c9ff03e586bf85a842268f881728be097d2e
hamza <hamzash863@gmail.com>
2022-08-04 18:37:00 +0300

    handle fetch error

--- src/components/Meals/AvailableMealis.js
+++ src/components/Meals/AvailableMealis.js
@@ -1,36 +1,41 @@
 import style from "./AvailableMealis.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 const AvailableMeals = () => {
   const [meailsData, setMeailsData] = useState([]);
   const [loading, setloading] = useState(false);
-  const [error, setError] = useState(null);
-  const fetchMeails = async () => {
-    try {
-      setloading(true);
-      const response = await fetch(
-        "https://tishreen-62882-default-rtdb.firebaseio.com/meals.json"
-      );
-      const data = await response.json();
-      console.log(data);
-      const loadedData = [];
-      for (var key in data) {
-        loadedData.push({
-          id: key,
-          ...data[key],
-        });
-      }
-      setMeailsData(loadedData);
-    } catch (e) {
-      setError(e);
+  const [error, setError] = useState();
+
+  const fetchMeails = useCallback(async () => {
+    setloading(true);
+    const response = await fetch(
+      "https://tishreen-62882-default-rtdb.firebaseio.com/meals.json"
+    );
+    if (!response.ok) {
+      setloading(false);
+      throw new Error("Some thing wrong bebe!!");
     }
+    const data = await response.json();
+    console.log(data);
+    const loadedData = [];
+    for (var key in data) {
+      loadedData.push({
+        id: key,
+        ...data[key],
+      });
+    }
+    setMeailsData(loadedData);
     setloading(false);
-  };
-  useEffect(() => {
-    fetchMeails();
   }, []);
+  useEffect(() => {
+    fetchMeails().catch((error) => {
+      setloading(false);
+      setError(error);
+    });
+  }, []);
+
   const meailsList = meailsData.map((meal) => (
     <MealItem
       key={meal.id}
@@ -40,10 +45,17 @@ const AvailableMeals = () => {
       price={meal.price}
     />
   ));
+  const content = loading ? (
+    <p>Loading ....</p>
+  ) : error != null ? (
+    <p>{error.message}</p>
+  ) : (
+    <ul>{meailsList}</ul>
+  );
 
   return (
     <section className={style.meals}>
-      <Card>{loading ? <p>Loading ....</p> : <ul>{meailsList}</ul>}</Card>
+      <Card>{content}</Card>
     </section>
   );
 };
